7c12e1e08b762df5334bb57de7e6e22278b33acc
Mario Enrico Ragucci <mario@whisperedshouts.de>
2020-05-06 10:30:48 +0200

    necessary changes to the WaffenObjekt
    Signed-off-by: Mario Enrico Ragucci <mario@whisperedshouts.de>

--- src/main/java/link/parzival/dsa/WaffenObjekt.java
+++ src/main/java/link/parzival/dsa/WaffenObjekt.java
@@ -3,21 +3,37 @@
  */
 package link.parzival.dsa;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author mario
  *
  */
 public class WaffenObjekt {
+	public enum Distanzklasse {
+		H,N,S,P
+	}
 	private String name;
 	private String trefferpunkte;
 	private int attacke;
 	private int parade;
 	private int initiative;
+	private int waffenModAttacke;
+	private int waffenModParade;
+	private List<Distanzklasse> distanzklassen = new ArrayList<>();
+	
 	/**
 	 * @return the attacke
 	 */
 	public int getAttacke() {
 		return attacke;
+	}
+	/**
+	 * @return the distanzklassen
+	 */
+	public List<Distanzklasse> getDistanzklassen() {
+		return distanzklassen;
 	}
 	/**
 	 * @return the initiative
@@ -44,10 +60,28 @@ public class WaffenObjekt {
 		return trefferpunkte;
 	}
 	/**
+	 * @return the waffenModAttacke
+	 */
+	public int getWaffenModAttacke() {
+		return waffenModAttacke;
+	}
+	/**
+	 * @return the waffenModParade
+	 */
+	public int getWaffenModParade() {
+		return waffenModParade;
+	}
+	/**
 	 * @param attacke the attacke to set
 	 */
 	public void setAttacke(int attacke) {
 		this.attacke = attacke;
+	}
+	/**
+	 * @param distanzklassen the distanzklassen to set
+	 */
+	public void setDistanzklassen(List<Distanzklasse> distanzklassen) {
+		this.distanzklassen = distanzklassen;
 	}
 	/**
 	 * @param initiative the initiative to set
@@ -72,6 +106,32 @@ public class WaffenObjekt {
 	 */
 	public void setTrefferpunkte(String trefferpunkte) {
 		this.trefferpunkte = trefferpunkte;
+	}
+	/**
+	 * @param waffenModAttacke the waffenModAttacke to set
+	 */
+	public void setWaffenModAttacke(int waffenModAttacke) {
+		this.waffenModAttacke = waffenModAttacke;
+	}
+	/**
+	 * @param waffenModParade the waffenModParade to set
+	 */
+	public void setWaffenModParade(int waffenModParade) {
+		this.waffenModParade = waffenModParade;
+	}
+	
+	public void addDistanzklasse(Distanzklasse distanzklasse) {
+		this.distanzklassen.add(distanzklasse);
+	}
+	
+	public String[] getDistanzklassenAsArray() {
+		int size = distanzklassen.size();
+		String[] result = new String[size];
+		for(int i = 0; i < size; i++) {
+			result[i] = distanzklassen.get(i).name();
+		}
+		
+		return result;
 	}
 	
 }
