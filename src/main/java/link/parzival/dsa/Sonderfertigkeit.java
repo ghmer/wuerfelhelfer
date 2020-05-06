380dc9e3ed8dc299a4e1780d996ed0794d53d384
Mario Enrico Ragucci <mario@whisperedshouts.de>
2020-05-06 10:30:36 +0200

    Adding new class for Sonderfertigkeiten
    Signed-off-by: Mario Enrico Ragucci <mario@whisperedshouts.de>

---
+++ src/main/java/link/parzival/dsa/Sonderfertigkeit.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package link.parzival.dsa;
+
+/**
+ * @author mario
+ *
+ */
+public class Sonderfertigkeit {
+	private String name;
+	
+	public Sonderfertigkeit() {
+		name = new String();
+	}
+	
+	public Sonderfertigkeit(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
