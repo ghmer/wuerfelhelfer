453b400cbb29ee3527fe7f5ed10928158e2b637e
Mario Enrico Ragucci <mario@whisperedshouts.de>
2020-05-20 04:40:19 +0200

    simple utility to check for a newer version
    Signed-off-by: Mario Enrico Ragucci <mario@whisperedshouts.de>

---
+++ src/main/java/link/parzival/dsa/VersionCheck.java
@@ -0,0 +1,64 @@
+/**
+ * 
+ */
+package link.parzival.dsa;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * @author mario
+ *
+ */
+public class VersionCheck {
+	public static final String VERSION_URL = "https://parzival.link/dz-helper-version.txt";
+	
+	/**
+	 * @param localVersion the localVersion of the program
+	 * @return true if there is a newer version on the remote site
+	 */
+	public static boolean checkForNewVersion(int localVersion) {
+		boolean result = false;
+		int remoteVersion = -1;
+		BufferedInputStream bis = null;
+		try {
+			URL url = new URL(VERSION_URL);
+			bis = new BufferedInputStream(url.openStream());
+			
+			byte dataBuffer[] = new byte[1024];
+		    String buffer = new String();
+		    @SuppressWarnings("unused")
+			int bytesRead;
+		    while ((bytesRead = bis.read(dataBuffer, 0, 1024)) != -1) {
+		    	String blubb = new String(dataBuffer);
+		    	buffer = buffer.concat(blubb);
+		    }
+		    
+		    if(buffer != null && !buffer.isEmpty()) {
+	    		remoteVersion = Integer.parseInt(buffer.trim());
+	    	}
+		    
+		} catch (MalformedURLException e) {
+			remoteVersion = -1;
+		} catch (IOException e) {
+			remoteVersion = -1;
+			System.err.println("File could not be found");
+		} finally {
+			if(bis != null) {
+				try {
+					bis.close();
+				} catch (IOException e) {
+					System.err.println(e.getMessage());
+				}
+			}
+		}
+		
+		if(remoteVersion != -1 && localVersion < remoteVersion) {
+			result = true;
+		}
+		
+		return result;
+	}
+}
