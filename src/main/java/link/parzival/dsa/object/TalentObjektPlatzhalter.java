31b3ad4d50c9a44e92b0d364b2824bc1bcd6741b
Mario Enrico Ragucci <mario@whisperedshouts.de>
2020-05-27 10:16:02 +0200

    Platzhalter Objekt
    Signed-off-by: Mario Enrico Ragucci <mario@whisperedshouts.de>

---
+++ src/main/java/link/parzival/dsa/object/TalentObjektPlatzhalter.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package link.parzival.dsa.object;
+
+import link.parzival.dsa.object.enumeration.EigenschaftEnum;
+
+/**
+ * @author mario
+ *
+ */
+public class TalentObjektPlatzhalter extends TalentObjekt {
+
+	/**
+	 * 
+	 */
+	public TalentObjektPlatzhalter() {
+		setBe("");
+		setName("Keine Probe gewählt");
+		setProbenTalent1(EigenschaftEnum.NA);
+		setProbenTalent2(EigenschaftEnum.NA);
+		setProbenTalent3(EigenschaftEnum.NA);
+		setTalentwert(0);
+	}
+
+}
