9db387b93abf1799947da5fca3666f0061f02501
Mario Enrico Ragucci <mario@whisperedshouts.de>
2020-05-31 19:31:19 +0200

    Versionsänderung
    Signed-off-by: Mario Enrico Ragucci <mario@whisperedshouts.de>

--- src/main/java/link/parzival/dsa/Constants.java
+++ src/main/java/link/parzival/dsa/Constants.java
@@ -11,10 +11,10 @@ public class Constants {
 	public static final String APP_URL 			= "https://parzival.link/dz-dice-helper-latest.jar";	
 	public static final String MANUAL_URL 		= "https://parzival.link/Wuerfelhelfer-Anleitung.pdf";
 	public static final String VERSION_URL 		= "https://parzival.link/dz-helper-version.txt";
-	public static final String VERSION_EXTERNAL = "0.6.2";
+	public static final String VERSION_EXTERNAL = "0.6.5";
 	
 	public static final int DIALOG_CANCEL_STATE = 1;
 	public static final int DIALOG_OK_STATE 	= 0;
 	
-	public  static final int VERSION 			= 15;	
+	public  static final int VERSION 			= 16;	
 }
