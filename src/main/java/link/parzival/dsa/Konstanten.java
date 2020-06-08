4c2ecfc5395852da548bcf744267ce51484814c1
Mario Enrico Ragucci <mario@whisperedshouts.de>
2020-06-09 00:45:18 +0200

    hotfix release 0.7.4
    Signed-off-by: Mario Enrico Ragucci <mario@whisperedshouts.de>

--- src/main/java/link/parzival/dsa/Konstanten.java
+++ src/main/java/link/parzival/dsa/Konstanten.java
@@ -23,11 +23,11 @@ public class Konstanten {
     /**
      * Nach außen angezeigte Version, matched mit der Version aus der pom.xml
      */
-    public static final String VERSION_EXTERNAL = "0.7.3";
+    public static final String VERSION_EXTERNAL = "0.7.4";
     /**
      * Intern verwendete Version, z.B. für Abfrage, ob eine neuere Version vorliegt
      */
-    public static final int VERSION             = 23;
+    public static final int VERSION             = 24;
     /**
      * Status, der von Modal-Dialogen zurückgegeben wird, falls der Cancel Button gedrückt wurde
      */
